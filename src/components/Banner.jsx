fb1e95f53ebe9429aef0e27fa5849510e8ad5060
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-07-15 13:24:00 -0300

    Update copy

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,7 @@ const Banner = () => {
         <>
             <div className="container-fluid banner-container py-5 my-5">
                 <div className="container text-center py-5">
-                    <p className="mb-5">Recibí las últimas noticias acerca de Cryptos, Trading, NTF's <br className="d-none d-md-block"/>llena el formulario y te contactaremos a la brevedad f</p>
+                    <p className="mb-5">Recibí las últimas noticias acerca de Cryptos, Trading, NTF's <br className="d-none d-md-block"/>llena el formulario y te contactaremos a la brevedad</p>
                     <a href="#">Registrarse</a>
                 </div>
             </div>
