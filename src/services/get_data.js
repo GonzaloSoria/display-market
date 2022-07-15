2cbff0840ef14e38f2885e407be659d37496a966
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-07-15 16:38:34 -0300

    fixing cors issues

--- src/services/get_data.js
+++ src/services/get_data.js
@@ -1,16 +1,10 @@
 const API_KEY = '52231c26044a9c4ef0dc98c0c3ada7b56836b2c5';
 const COINS = ['BTC','ETH','BNB','DAI']; //Usar 4 como preferidas para no romper el layout
 const SERVER_URL = `https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=${COINS}&convert=ARS`;
-const CORS_ANYWHERE = 'https://cors-anywhere.herokuapp.com/';
 
 export const get_data = async (setState) => {
-    const api = await fetch(CORS_ANYWHERE + SERVER_URL, {
-        method: 'GET',
-        //mode: 'no-cors',
-        headers: new Headers({
-            'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*',
-        }),
+    const api = await fetch(SERVER_URL, {
+        mode: 'cors'
     });
     try {
         const data_parsed = await api.json();
