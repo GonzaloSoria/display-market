addedb29f7be042d6b93a35f207eccd187ff6cc3
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-07-18 12:36:42 -0300

    fixing cors issues

--- src/components/Coin.jsx
+++ src/components/Coin.jsx
@@ -1,17 +1,19 @@
 const Coin = ({currency, price, image, price_change_24hs}) => {
     // Formatear el precio a moneda argentina
     const price_ars = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(price);
+    const price_ars_changed = new Intl.NumberFormat('es-AR', { style: 'currency', currency: 'ARS' }).format(price_change_24hs);
+
 
     return (
         <>
-            <div className="coin-box d-flex justify-content-center align-items-center px-5 my-3">
+            <div className="coin-box d-flex justify-content-start align-items-center px-5 my-3">
                 <div>
                     <img src={image} alt={currency} />
                 </div>
-                <div className="ps-5 py-2 data-coin-container">
+                <div className="w-70 ps-5 py-2 data-coin-container">
                     <p className="mb-0 price">{price_ars}</p>
                     <p className="mb-0 currency">{currency}</p>
-                    <p className={price_change_24hs > 0 ? 'text-green' : 'text-red'}>{price_change_24hs}%</p>
+                    <p className={price_change_24hs > 0 ? 'text-green' : 'text-red'}>{price_ars_changed}%</p>
                 </div>
             </div>
             
--- src/services/get_data.js
+++ src/services/get_data.js
@@ -1,5 +1,8 @@
+const PARAM = '%2C';
+const POPULAR_COINS = ['bitcoin', PARAM, 'ethereum', PARAM, 'tether', PARAM, 'binancecoin'];
+
 export const get_data = async (setState) => {
-    const api = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&ids=bitcoin%2Cethereum%2Cbinancecoin%2Cdai&order=market_cap_desc');
+    const api = await fetch(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=ars&ids=${POPULAR_COINS}&order=market_cap_desc`);
     try {
         const data_parsed = await api.json();
         setState(data_parsed);
