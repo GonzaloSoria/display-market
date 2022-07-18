035557e20a318a9e7531754637f2e673cc908066
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-07-18 11:37:08 -0300

    adding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,12 @@
 const Footer = () => {
     return (
         <>
-            <section className="container-fluid footer-container">
+            <footer className="container-fluid footer-container">
                 <div className="container text-center">
                     <h2 className="mb-3">Displaymarket</h2>
                     <p>Todos los Derechos Reservados. Prohibida su reproducción total o parcial. Los usuarios se obligan a cumplir con los Términos y Condiciones del Sitio de Displaymarket. Copyright © 2022.</p>
                 </div>
-            </section>
+            </footer>
         </>
     );
 };
