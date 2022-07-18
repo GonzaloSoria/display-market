9603ade552b9bf52040b3a9e0b7dc8b8b9fbe24a
GonzaloSoria <gonzalo.sooria@gmail.com>
2022-07-18 10:58:59 -0300

    example

--- src/services/get_data.js
+++ src/services/get_data.js
@@ -2,21 +2,25 @@ const API_KEY = '52231c26044a9c4ef0dc98c0c3ada7b56836b2c5';
 const COINS = ['BTC','ETH','BNB','DAI']; //Usar 4 como preferidas para no romper el layout
 const SERVER_URL = `https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=${COINS}&convert=ARS`;
 
+fetch(`https://api.nomics.com/v1/currencies/ticker?key=${API_KEY}&ids=${COINS}&interval=1d,30d&convert=EUR&platform-currency=ETH&per-page=100&page=1`)
+  .then(response => response.json())
+  .then(data => console.log(data))
+
 export const get_data = async (setState) => {
-    const api = await fetch(SERVER_URL, {
-        status: 200,
-        headers: {
-            'Acces-Control-Allow-Headers': 'Content-Type',
-            'Acces-Control-Allow-Origin': 'https://landing-page-challenge-three.vercel.app/',
-            'Acces-Control-Allow-Methods': 'GET',
-        }
-    });
-    console.log(api);
-    try {
-        const data_parsed = await api.json();
-        setState(data_parsed);
-        console.log(data_parsed);
-    } catch (error) {
-        console.log(error);
-    }
+    // const api = await fetch(SERVER_URL, {
+    //     status: 200,
+    //     headers: {
+    //         'Acces-Control-Allow-Headers': 'Content-Type',
+    //         'Acces-Control-Allow-Origin': 'https://landing-page-challenge-three.vercel.app/',
+    //         'Acces-Control-Allow-Methods': 'GET',
+    //     }
+    // });
+    // console.log(api);
+    // try {
+    //     const data_parsed = await api.json();
+    //     setState(data_parsed);
+    //     console.log(data_parsed);
+    // } catch (error) {
+    //     console.log(error);
+    // }
 };
